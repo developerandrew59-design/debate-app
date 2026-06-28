64b282040fd1017f8cd565984b11a7b0e281b42b
developerandrew59-design <developerandrew59@gmail.com>
2026-06-28 11:28:31 +0530

    added 2 tables more to have different types of clubs with different features

--- models.py
+++ models.py
@@ -8,6 +8,16 @@ class ClubType(str,enum.Enum):
     private="private"
     one_v_one="1v1"
 
+class Status(str,enum.Enum):
+    pending="pending"
+    accepted="accepted"
+    rejected="rejected"  
+
+
+class RequestType(str,enum.Enum):
+    request="request"
+    invite="invite"      
+
 class User(Base):
     __tablename__="accounts"
     id=Column(Integer,primary_key=True,nullable=False)
@@ -19,7 +29,7 @@ class Debate(Base):
     __tablename__="club"
     id=Column(Integer,primary_key=True,nullable=False)
     name=Column(String,nullable=False)
-    club_type=Column(Enum(ClubType),nullable=False,default=ClubType.private)
+    club_type=Column(Enum(ClubType),nullable=False,default=ClubType.public)
     created_at=Column(TIMESTAMP(timezone=True),nullable=False,server_default="now()")
 
 class Argument(Base):
@@ -40,3 +50,20 @@ class Vote(Base):
     argument_id=Column(Integer,ForeignKey("arguments.id",ondelete="CASCADE"),nullable=False) 
     created_at=Column(TIMESTAMP(timezone=True),nullable=False,server_default="now()")
 
+
+class Club_members(Base):
+    __tablename__="club_members"
+    id=Column(Integer,primary_key=True,nullable=False)
+    account_id=Column(Integer,ForeignKey("accounts.id",ondelete="CASCADE"),nullable=False)
+    club_id=Column(Integer,ForeignKey("club.id",ondelete="CASCADE"),nullable=False)
+    joined_at=Column(TIMESTAMP(timezone=True),nullable=False,server_default="now()")
+
+
+class Club_Request(Base):
+    __tablename__="club_requests"
+    id=Column(Integer,primary_key=True,nullable=False)
+    account_id=Column(Integer,ForeignKey("accounts.id",ondelete="CASCADE"),nullable=False)
+    club_id=Column(Integer,ForeignKey("club.id",ondelete="CASCADE"),nullable=False)
+    status=Column(Enum(Status),nullable=False,default=Status.pending)
+    request_type=Column(Enum(RequestType),nullable=False)
+    created_at=Column(TIMESTAMP(timezone=True),nullable=False,server_default="now()")
